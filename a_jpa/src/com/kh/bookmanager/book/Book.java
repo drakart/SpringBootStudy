fdc6cc7a938b1413555dd31e47ad4d7bfec1aa1b
HyeonSoo <57429706+drakart@users.noreply.github.com>
2021-10-29 16:18:01 +0900

    21/10/29 JPA 프로젝트 설정 및 JPQL 실습

---
+++ a_jpa/src/com/kh/bookmanager/book/Book.java
@@ -0,0 +1,38 @@
+package com.kh.bookmanager.book;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+
+import lombok.Data;
+
+@Entity
+@DynamicUpdate //변경이 감지된 속성만 쿼리에 반영
+@DynamicInsert //값이 null이 아닌 속성만 쿼리에 반영 
+@Data
+public class Book {
+
+	@Id
+	@GeneratedValue // JPA정책에 따라 식별자를 자동으로 생성
+	private Long bkIdx;
+	private String isbn;
+	private String category;
+	private String title;
+	private String author;
+	private String info;
+	@Column(columnDefinition = "number default 1")
+	private Integer bookAmt;
+	@Column(columnDefinition = "date default sysdate")
+	private Date regDate;
+	@Column(columnDefinition = "number default 0")
+	private Integer rentCnt;
+	
+	
+}
